fea1cde9fee4a1c2e88baf2be2c7efb80365ed1e
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-01-30 12:30:19 +0800

    1 file modified

--- prelims/Lab-Activity-3/server.js
+++ prelims/Lab-Activity-3/server.js
@@ -6,24 +6,29 @@ var http = require('http');
 // Import Node.js core module
 
 var server = http.createServer (function (req, res) { 
-  //create web server if (req.url='/') { //check the URL of the current request
+  //create web server 
+  if (req.url='/') { //check the URL of the current request
 
   // set response header
   res.writeHead (200, {'Content-Type': 'text/html' });
 
 
   // set response content
-  res.write('<html><body> <p>This is home Page.</p> </body></html>"); 
+  res.write('<html><body> <p>This is home Page.</p> </body></html>');
   res.end();
 
   }
-  else if (req.url="/student") {
-    res.writeHead (200, {'Content-Type': 'text/html' }); res.write('<html><body> <p>This is student Page.</p> </body></html>"); 
+  else if (req.url == "/student") {
+
+    res.writeHead (200, { 'Content-Type': 'text/html' }); 
+    res.write('<html><body> <p>This is admin Page.</p></body></html>'); 
     res.end();
 
   }
-  else if (req.url = "/admin") {
-    res.writeHead (200, ('Content-Type': 'text/html" }); res.write('<html><body> <p>This is admin Page.</p></body></html>"); 
+  else if (req.url == "/admin") {
+
+    res.writeHead (200, { 'Content-Type': 'text/html' }); 
+    res.write('<html><body> <p>This is admin Page.</p></body></html>'); 
     res.end();
     
   }
@@ -34,4 +39,4 @@ var server = http.createServer (function (req, res) {
 
 server.listen(5000); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..")
+console.log('Node.js web server at port 5000 is running..')
