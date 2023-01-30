e6a2dc160b7ac7ce2409470be1b9779e51f60e0e
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-01-30 12:53:29 +0800

    1 file modified

--- prelims/Lab-Activity-3/server.js
+++ prelims/Lab-Activity-3/server.js
@@ -14,7 +14,8 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // set response content
-    res.write('<html><body> <p>This is home Page.</p> </body></html>');
+    res.write('<html><body> <h1>Welcom to my Node.js Application</h1> </body></html>');
+    res.write('<html><body> <p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>');
     res.end();
   } else if (req.url == '/student') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
