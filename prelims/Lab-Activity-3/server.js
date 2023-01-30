2c0fcf1d63320c02c5a21add503ba309ce82e3ab
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-01-30 12:35:28 +0800

    1 file modified

--- prelims/Lab-Activity-3/server.js
+++ prelims/Lab-Activity-3/server.js
@@ -21,7 +21,7 @@ var server = http.createServer (function (req, res) {
   else if (req.url == "/student") {
 
     res.writeHead (200, { 'Content-Type': 'text/html' }); 
-    res.write('<html><body> <p>This is admin Page.</p></body></html>'); 
+    res.write('<html><body> <p>This is student Page.</p></body></html>'); 
     res.end();
 
   }
