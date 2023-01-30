d8f04b2ec1335eec492a7294028f64294ce305ac
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-01-30 12:11:23 +0800

    1 file modified

--- prelims/Lab-Activity-3/server.js
+++ prelims/Lab-Activity-3/server.js
@@ -2,14 +2,14 @@
 // Author: Sean Warner Dean T. Datu
 // Section: WD - 201
 
-var http = require('http'); 
+var http = require('http');
 // 1 Import Node.js core module
 
-var server http.createServer (function (req, res) { // 2 creating server
-//handle incomming requests here..
-
+var server = http.createServer(function (req, res) {
+  // 2 creating server
+  //handle incomming requests here..
 });
 
 server.listen(5000); //3 listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+console.log('Node.js web server at port 5000 is running..');
