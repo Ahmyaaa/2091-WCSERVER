99d2a17029a2f24b05cc1a0128939a7ecdfbf65b
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-01-30 12:07:30 +0800

    2 files modified

--- prelims/Person/Person.js
+++ prelims/Person/Person.js
@@ -2,6 +2,6 @@ module.exports = function (firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.fullName = function () {
-    return this.firstName + ' ' +this.lastName;
+    return this.firstName + ' ' + this.lastName;
   }
 }
