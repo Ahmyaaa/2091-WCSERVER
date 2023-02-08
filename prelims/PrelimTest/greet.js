59e3e76a46b0408059d3ad6de68d3e3bf0b3d031
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-02-08 15:51:45 +0800

    2 files added

---
+++ prelims/PrelimTest/greet.js
@@ -0,0 +1,13 @@
+function greet(name){
+  return "Hello! , " + name
+}
+
+function greet(name){
+  return "Congratulation! , " + name
+}
+
+function greet(name){
+  return "Goodbye! , " + name
+}
+
+module.exports = greet;
